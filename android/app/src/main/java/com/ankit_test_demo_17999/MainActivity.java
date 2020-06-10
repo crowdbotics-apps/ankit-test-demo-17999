377192cc607ab91a1f3f238e9937d42833937d7d
Crowdbotics <team@crowdbotics.com>
2020-06-10 18:22:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ankit_test_demo_17999/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ankit_test_demo_17999;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ankit_test_demo_17999";
+    }
+}
