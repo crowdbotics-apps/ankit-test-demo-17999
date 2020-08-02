e903a25a04526763fce03fd90a10a3386e68806f
Crowdbotics <team@crowdbotics.com>
2020-08-02 17:28:19 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'CopyOfFeed5686890', human_name: 'Copy of Feed 56', access_route: 'CopyOfFeed5686890'},
 { name: 'CopyOfFeed5686889', human_name: 'Copy of Feed 56', access_route: 'CopyOfFeed5686889'},
 { name: 'CopyOfFeed5686888', human_name: 'Copy of Feed 56', access_route: 'CopyOfFeed5686888'},
 { name: 'CopyOfFeed5686887', human_name: 'Copy of Feed 56', access_route: 'CopyOfFeed5686887'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import CopyOfFeed5686890Navigator from '../features/CopyOfFeed5686890/navigator';
 import CopyOfFeed5686889Navigator from '../features/CopyOfFeed5686889/navigator';
 import CopyOfFeed5686888Navigator from '../features/CopyOfFeed5686888/navigator';
 import CopyOfFeed5686887Navigator from '../features/CopyOfFeed5686887/navigator';
@@ -31,6 +32,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+CopyOfFeed5686890: { screen: CopyOfFeed5686890Navigator },
 CopyOfFeed5686889: { screen: CopyOfFeed5686889Navigator },
 CopyOfFeed5686888: { screen: CopyOfFeed5686888Navigator },
 CopyOfFeed5686887: { screen: CopyOfFeed5686887Navigator },
